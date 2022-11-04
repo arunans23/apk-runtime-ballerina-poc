61b841269312e44f6df6931b20f76ca44f11e12f
Arunan <arunans23@gmail.com>
2022-11-04 19:18:25 +0530

    Update script

--- resources/extract_token.sh
+++ resources/extract_token.sh
@@ -1,4 +1,3 @@
-#bin/bash
-SERVICE_ACCOUNT=api-explorer
-K8S_SERVICE_TOKEN=$(kubectl create token ${SERVICE_ACCOUNT})
-K8S_API_ENDPOINT=https://$(kubectl -n default get endpoints kubernetes --no-headers | awk '{ print $2 }')
+export SERVICE_ACCOUNT=api-explorer
+export K8S_SERVICE_TOKEN=$(kubectl create token ${SERVICE_ACCOUNT})
+export K8S_API_SERVER_URL=https://$(kubectl -n default get endpoints kubernetes --no-headers | awk '{ print $2 }')
